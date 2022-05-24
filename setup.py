9b1c596c798c53be5b52e13160e8a0ffcd465c4f
JJ Uzumaki <104718343+xpunk7@users.noreply.github.com>
2022-05-24 13:21:29 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='Exoswap',
+    description='Exoswap Automated Market Maker',
+    long_description=readme,
+    author='JJ Uzumaki',
+    author_email='jjuzumaki@proton.me',
+    license=license,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'bnb',
+        'web3',
+        'py-solc',
+        'pytest'
+    ],
+)
